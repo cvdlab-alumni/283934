329b2a1eaee5ee8668463bf790f2da4f8bd6b9b8
krelian87 <maffo.anima87@gmail.com>
2013-03-28 19:29:11 +0100

    updated folder

--- 2013-03-28/2013-03-28.js
+++ 2013-03-28/2013-03-28.js
@@ -12,22 +12,21 @@ function Triangle(a,b,c){
 	this.a = a;
 	this.b = b;
 	this.c = c;
-	
-	this.area = function(){
-		var e1 = new Edge(a,b); 
-		var e2 = new Edge(b,c);
-		var e3 = new Edge(a,c);
-		var p = this.perimeter()/2;
-		return Math.sqrt(p*(p-e1.length())*(p-e2.length())*(p-e3.length()));
-	};
 }
 
 
 //add length to Edge
-Edge.prototype.length = function() {return Math.sqrt(Math.pow((this.v.x-this.u.x),2)+Math.pow((this.v.y-this.u.y),2));};
+Edge.prototype.length = function() {
+		return Math.sqrt(Math.pow((this.v.x-this.u.x),2)+Math.pow((this.v.y-this.u.y),2));
+};
 
 //add perimeter to Triangle
-Triangle.prototype.perimeter = function() {return this.a.length()+this.b.length()+this.c.length();};
+Triangle.prototype.perimeter = function() {
+		return this.a.length()+this.b.length()+this.c.length();
+};
 
 //add area to Triangle
-Triangle.prototype.area = function() {var p = this.perimeter()/2; return Math.sqrt(p*(p-this.a.length())*(p-this.b.length())*(p-this.c.length()));};
+Triangle.prototype.area = function() {
+		var p = this.perimeter()/2; 
+		return Math.sqrt(p*(p-this.a.length())*(p-this.b.length())*(p-this.c.length()));
+};
